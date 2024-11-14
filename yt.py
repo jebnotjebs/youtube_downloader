f7046f5b319f70773c62fb0514817f0e35980532
Johndel Barcelona <80611371+jebnotjebs@users.noreply.github.com>
2024-11-14 14:26:56 +0800

    ss

--- yt.py
+++ yt.py
@@ -63,3 +63,7 @@ def download_video():
         return jsonify({"status": "error", "message": "Stream not available"})
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3000)
